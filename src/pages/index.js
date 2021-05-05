df4014c3fca941fa002a50afeceb95b98914815a
RosemaryLy <rosey.ly@hotmail.com>
2021-05-05 12:44:55 -0400

    react-bootstrap for fast design development

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Container} from 'react-bootstrap';
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
@@ -11,9 +12,9 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    <Container>
       <Image />
-    </div>
+    </Container>
     <Link to="/page-2/">Go to page 2</Link><br/>
     <Link to="/aboutMe/">About Me</Link>
   </Layout>
